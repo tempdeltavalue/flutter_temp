b28be408eab358b9a13edd55816d71cca03617d6
tempdeltavalue <mykhailo.mel1122@gmail.com>
2023-12-07 11:07:25 +0200

    minor

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,7 +12,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Text('Меню')),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
